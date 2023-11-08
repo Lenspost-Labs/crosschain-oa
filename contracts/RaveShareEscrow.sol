74085f6319016fbc453e2726ad6a707fc7e77ac1
aryan <agarwalaryan139@gmail.com>
2023-11-08 16:28:56 +0530

    fix: duplication

--- contracts/RaveShareEscrow.sol
+++ contracts/RaveShareEscrow.sol
@@ -1,28 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-struct SalesConfiguration {
-    uint104 publicSalePrice;
-    uint32 maxSalePurchasePerAddress;
-    uint64 publicSaleStart;
-    uint64 publicSaleEnd;
-    uint64 presaleStart;
-    uint64 presaleEnd;
-    bytes32 presaleMerkleRoot;
-}
-
-struct DropParams {
-    string name;
-    string symbol;
-    address defaultAdmin;
-    uint64 editionSize;
-    uint16 royaltyBPS;
-    address payable fundsRecipient;
-    SalesConfiguration saleConfig;
-    string metadataURIBase;
-    string metadataContractURI;
-}
-
 import "./IZoraNFTCreatorV1/IZoraNFTCreatorV1.sol";
 
 /// @dev RaveShare Escrow Proxy Access Contract
