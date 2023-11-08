28286813a84d7866d4adafbd090facaf1bb8efce
aryan <agarwalaryan139@gmail.com>
2023-11-08 16:55:36 +0530

    fix: more issues

--- contracts/RaveShareEscrow.sol
+++ contracts/RaveShareEscrow.sol
@@ -3,20 +3,14 @@ pragma solidity ^0.8.20;
 
 import "./IZoraNFTCreatorV1/IZoraNFTCreatorV1.sol";
 
-/// @dev RaveShare Escrow Proxy Access Contract
 contract RaveShareEscrowProxy {
     address public zoraNFTCreatorContract =
         0xfFFD7409031B1aeb731271C6C2D59771523Ff8a8;
 
     IZoraNFTCreatorV1 public zora = IZoraNFTCreatorV1(zoraNFTCreatorContract);
 
-    // function mintNFT() public returns (uint256) {
-    //     uint256 result = zora.createDrop();
-    //     require(result == 0, "RaveShareEscrowProxy: mintNFT failed");
-    //     return result;
-    // }
-
     function createNFT(DropParams calldata params) public returns (address) {
+        IZoraNFTCreatorV1.SalesConfiguration memory salesConfig = params.saleConfig;
         // Call the external contract's createDrop function with the struct
         address newNFTContract = zora.createDrop(
             params.name,
@@ -25,7 +19,7 @@ contract RaveShareEscrowProxy {
             params.editionSize,
             params.royaltyBPS,
             params.fundsRecipient,
-            params.saleConfig,
+            salesConfig,
             params.metadataURIBase,
             params.metadataContractURI
         );
